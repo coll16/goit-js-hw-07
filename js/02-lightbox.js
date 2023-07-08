8beb2a74754f2c9631b7015b6a2db6e236921bcf
coll16 <126190508+coll16@users.noreply.github.com>
2023-07-08 15:48:38 +0300

    hw7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const createGalleryItemsMarkup = ({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+      </a>
+    </li>
+  `;
+};
+
+const galleryItemsMarkup = galleryItems
+  .map(item => createGalleryItemsMarkup(item))
+  .join('');
+
+galleryRef.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+
+const images = galleryRef.querySelectorAll('.gallery__image');
+images.forEach(image => {
+  const altText = image.getAttribute('alt');
+  image.setAttribute('title', altText);
+});
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'title',
+});
+
